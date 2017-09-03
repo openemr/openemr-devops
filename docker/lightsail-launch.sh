c1e38b9a5d5e9d4b7323833d0f5ad7e61bf5be19
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-09-03 15:19:27 -0500

    first try

--- docker/lightsail-launch.sh
+++ docker/lightsail-launch.sh
@@ -9,7 +9,6 @@ apt-get install -y apt-transport-https ca-certificates git
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-#apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get install -y docker-engine
 service docker start
 
