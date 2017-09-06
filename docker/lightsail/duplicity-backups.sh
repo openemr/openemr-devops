77b3ee3ab5bde9d083c1c650149041ce7c53a49b
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-09-06 00:42:06 -0500

    end of day

--- docker/lightsail/duplicity-backups.sh
+++ docker/lightsail/duplicity-backups.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker exec $(docker ps | grep mysql | cut -f 1 -d " ") /root/xtra-backup.sh
-duplicity --no-encryption --full-if-older-than 7D --include $(docker volume inspect docker_sitevolume | jq -r ".[0].Mountpoint") --include $(docker volume inspect docker_sitevolume | jq -r ".[0].Mountpoint") --exclude '**' / file:///root/backups/
+duplicity --no-encryption --full-if-older-than 7D --include $(docker volume inspect docker_sqlbackup | jq -r ".[0].Mountpoint") --include $(docker volume inspect docker_sitevolume | jq -r ".[0].Mountpoint") --exclude '**' / file:///root/backups/
 duplicity remove-all-but-n-full 2 --force file:///root/backups/
--- docker/mysql/xtra-backup.sh
+++ docker/mysql/xtra-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # TODO: use bash template to avoid hardcoded password
+# TODO should some of these be xtrabackup calls?
+# TODO: adopt https://github.com/dotmanila/mootools/blob/master/xbackup.sh
 innobackupex --no-timestamp --password=root /mnt/backups | tee /mnt/backups/backup-logs.txt
 innobackupex --apply-log --use-memory 25000000 /mnt/backups | tee --append /mnt/backups/backup-logs.txt
 echo backup process complete!
