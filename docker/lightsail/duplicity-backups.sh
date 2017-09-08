e8df71d8c5ef930d402a486868ef3bc64bf34ad4
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-09-08 00:09:30 -0500

    better

--- docker/lightsail/duplicity-backups.sh
+++ docker/lightsail/duplicity-backups.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ $(date +%u) == 7 ]; then
-  docker exec $(docker ps | grep mysql | cut -f 1 -d " ") /root/xbackup.sh -t full -f
-else
-  docker exec $(docker ps | grep mysql | cut -f 1 -d " ") /root/xbackup.sh -t incr
-fi;
+docker exec $(docker ps | grep mysql | cut -f 1 -d " ") /root/xbackup-wrapper.sh
 duplicity --no-encryption --full-if-older-than 9D --include $(docker volume inspect docker_sqlbackup | jq -r ".[0].Mountpoint") --include $(docker volume inspect docker_sitevolume | jq -r ".[0].Mountpoint") --exclude '**' / file:///root/backups/
 duplicity remove-all-but-n-full 2 --force file:///root/backups/
