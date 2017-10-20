ff7a0f3732d2f89623aa66111a7ad342a4018cf3
Asher Densmore-Lynn <jesdynf@gmail.com>
2017-10-20 00:41:14 -0500

    reorg
    missed this bit

--- stacks/single-server/utilities/ami-lock.sh
+++ stacks/single-server/utilities/ami-lock.sh
@@ -4,3 +4,5 @@
 # manual entry of MySQL root password is required
 
 docker exec -it $(docker ps | grep mysql | cut -f 1 -d " ") mysql -p -e "update openemr.users set active=0 where id=1;"
+cp ami-rekey.sh /etc/init.d/ami-rekey
+chkconfig --add ami-rekey
