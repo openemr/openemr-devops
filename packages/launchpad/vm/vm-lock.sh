d598247d3e5c5d76cef8cfe52519d89da08075d2
Asher Densmore-Lynn <jesdynf@gmail.com>
2018-05-24 20:59:47 -0500

    bugfix

--- packages/launchpad/vm/vm-lock.sh
+++ packages/launchpad/vm/vm-lock.sh
@@ -4,5 +4,5 @@
 # manual entry of MySQL root password is required
 
 docker exec -it $(docker ps | grep mysql | cut -f 1 -d " ") mysql -p -e "update openemr.users set active=0 where id=1;"
-cp ami-rekey.sh /etc/init.d/vm-rekey
+cp vm-rekey.sh /etc/init.d/vm-rekey
 update-rc.d vm-rekey defaults
