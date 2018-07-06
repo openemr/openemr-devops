78287ab683b0cd3d99065c7e2918df6cc999ed49
Brady Miller <brady.g.miller@gmail.com>
2018-07-06 09:06:42 -0700

    add SWARM_MODE to the flex and flex-edge dockers

--- docker/openemr/flex/autoconfig.sh
+++ docker/openemr/flex/autoconfig.sh
@@ -13,41 +13,15 @@
 #    - Setting openemr parameters OE_USER, OE_PASS
 set -e
 
-# ensure a self-signed cert has been generated and is referenced
-if ! [ -f /etc/ssl/private/selfsigned.key.pem ]; then
-    openssl req -x509 -newkey rsa:4096 \
-    -keyout /etc/ssl/private/selfsigned.key.pem \
-    -out /etc/ssl/certs/selfsigned.cert.pem \
-    -days 365 -nodes \
-    -subj "/C=xx/ST=x/L=x/O=x/OU=x/CN=localhost"
-fi
-rm -f /etc/ssl/certs/webserver.cert.pem
-rm -f /etc/ssl/private/webserver.key.pem
-ln -s /etc/ssl/certs/selfsigned.cert.pem /etc/ssl/certs/webserver.cert.pem
-ln -s /etc/ssl/private/selfsigned.key.pem /etc/ssl/private/webserver.key.pem
-
-if [ "$DOMAIN" != "" ]; then
-    if [ "$EMAIL" != "" ]; then
-        EMAIL="-m $EMAIL"
+swarm_wait() {
+    if [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-completed ]; then
+        # true
+        return 0
     else
-        echo "WARNING: SETTING AN EMAIL VIA \$EMAIL is HIGHLY RECOMMENDED IN ORDER TO"
-        echo "         RECEIVE ALERTS FROM LETSENCRYPT ABOUT YOUR SSL CERTIFICATE."
+        # false
+        return 1
     fi
-    # if a domain has been set, set up LE and target those certs
-    if ! [ -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
-        /usr/sbin/httpd -k start
-        sleep 2
-        certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d $DOMAIN $EMAIL --agree-tos
-        /usr/sbin/httpd -k stop
-        echo "1 23  *   *   *   certbot renew -q --post-hook \"httpd -k graceful\"" >> /etc/crontabs/root
-    fi
-
-    # run letsencrypt as a daemon and reference the correct cert
-    rm -f /etc/ssl/certs/webserver.cert.pem
-    rm -f /etc/ssl/private/webserver.key.pem
-    ln -s /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/ssl/certs/webserver.cert.pem
-    ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/ssl/private/webserver.key.pem
-fi
+}
 
 auto_setup() {
 
@@ -82,96 +56,164 @@ auto_setup() {
     fi
 }
 
-if [ -f /var/www/localhost/htdocs/auto_configure.php ] &&
-   [ "$EMPTY" != "yes" ] ; then
-    echo "Configuring a new flex openemr docker"
-    if [ "$FLEX_REPOSITORY" == "" ]; then
-        echo "Exiting from OpenEMR flex docker since missing required FLEX_REPOSITORY environment setting."
-        exit 1
-    fi
-    if [ "$FLEX_REPOSITORY_BRANCH" == "" ] &&
-       [ "$FLEX_REPOSITORY_TAG" == "" ]; then
-        echo "Exiting from OpenEMR flex docker since missing required FLEX_REPOSITORY_BRANCH or FLEX_REPOSITORY_TAG environment setting."
-        exit 1
+if [ "$SWARM_MODE" == "yes" ]; then
+    # Need to support replication for docker orchestration
+    if [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-initiated ]; then
+        # This docker instance will be the leader and perform configuration
+        touch /var/www/localhost/htdocs/openemr/sites/default/docker-initiated
+        touch /etc/docker-leader
     fi
 
-    cd /
-
-    if [ "$FLEX_REPOSITORY_BRANCH" != "" ]; then
-        echo "Collecting $FLEX_REPOSITORY_BRANCH branch from $FLEX_REPOSITORY repository"
-        git clone "$FLEX_REPOSITORY" --branch "$FLEX_REPOSITORY_BRANCH" --depth 1
-    else
-        echo "Collecting $FLEX_REPOSITORY_TAG tag from $FLEX_REPOSITORY repository"
-        git clone "$FLEX_REPOSITORY"
-        cd openemr
-        git checkout "$FLEX_REPOSITORY_TAG"
-        cd ../
+    if [ ! -f /etc/docker-leader ] &&
+       [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-completed ]; then
+        while swarm_wait; do
+            echo "Waiting for the docker-leader to finish configuration before proceeding."
+            sleep 10;
+        done
     fi
-    rsync --recursive --links --exclude .git openemr /var/www/localhost/htdocs/
-    rm -fr openemr
-    cd /var/www/localhost/htdocs/
 fi
 
-if [ -f /var/www/localhost/htdocs/auto_configure.php ]; then
-    chmod 666 /var/www/localhost/htdocs/openemr/sites/default/sqlconf.php
-    chmod 666 /var/www/localhost/htdocs/openemr/interface/modules/zend_modules/config/application.config.php
-    chown -R apache /var/www/localhost/htdocs/openemr/
+if [ -f /etc/docker-leader ] ||
+   [ "$SWARM_MODE" != "yes" ]; then
+    # ensure a self-signed cert has been generated and is referenced
+    if ! [ -f /etc/ssl/private/selfsigned.key.pem ]; then
+        openssl req -x509 -newkey rsa:4096 \
+        -keyout /etc/ssl/private/selfsigned.key.pem \
+        -out /etc/ssl/certs/selfsigned.cert.pem \
+        -days 365 -nodes \
+        -subj "/C=xx/ST=x/L=x/O=x/OU=x/CN=localhost"
+    fi
+    rm -f /etc/ssl/certs/webserver.cert.pem
+    rm -f /etc/ssl/private/webserver.key.pem
+    ln -s /etc/ssl/certs/selfsigned.cert.pem /etc/ssl/certs/webserver.cert.pem
+    ln -s /etc/ssl/private/selfsigned.key.pem /etc/ssl/private/webserver.key.pem
+
+    if [ "$DOMAIN" != "" ]; then
+        if [ "$EMAIL" != "" ]; then
+            EMAIL="-m $EMAIL"
+        else
+            echo "WARNING: SETTING AN EMAIL VIA \$EMAIL is HIGHLY RECOMMENDED IN ORDER TO"
+            echo "         RECEIVE ALERTS FROM LETSENCRYPT ABOUT YOUR SSL CERTIFICATE."
+        fi
+        # if a domain has been set, set up LE and target those certs
+        if ! [ -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
+            /usr/sbin/httpd -k start
+            sleep 2
+            certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d $DOMAIN $EMAIL --agree-tos
+            /usr/sbin/httpd -k stop
+            echo "1 23  *   *   *   certbot renew -q --post-hook \"httpd -k graceful\"" >> /etc/crontabs/root
+        fi
+
+        # run letsencrypt as a daemon and reference the correct cert
+        rm -f /etc/ssl/certs/webserver.cert.pem
+        rm -f /etc/ssl/private/webserver.key.pem
+        ln -s /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/ssl/certs/webserver.cert.pem
+        ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/ssl/private/webserver.key.pem
+    fi
 fi
 
-CONFIG=$(php -r "require_once('/var/www/localhost/htdocs/openemr/sites/default/sqlconf.php'); echo \$config;")
-if [ "$CONFIG" == "0" ] &&
-   [ "$MYSQL_HOST" != "" ] &&
-   [ "$MYSQL_ROOT_PASS" != "" ] &&
-   [ "$EMPTY" != "yes" ] &&
-   [ "$MANUAL_SETUP" != "yes" ]; then
+if [ -f /etc/docker-leader ] ||
+   [ "$SWARM_MODE" != "yes" ]; then
+    if [ -f /var/www/localhost/htdocs/auto_configure.php ] &&
+       [ "$EMPTY" != "yes" ] ; then
+        echo "Configuring a new flex openemr docker"
+        if [ "$FLEX_REPOSITORY" == "" ]; then
+            echo "Exiting from OpenEMR flex docker since missing required FLEX_REPOSITORY environment setting."
+            exit 1
+        fi
+        if [ "$FLEX_REPOSITORY_BRANCH" == "" ] &&
+           [ "$FLEX_REPOSITORY_TAG" == "" ]; then
+            echo "Exiting from OpenEMR flex docker since missing required FLEX_REPOSITORY_BRANCH or FLEX_REPOSITORY_TAG environment setting."
+            exit 1
+        fi
 
-    echo "Running quick setup!"
-    while ! auto_setup; do
-        echo "Couldn't set up. Any of these reasons could be what's wrong:"
-        echo " - You didn't spin up a MySQL container or connect your OpenEMR container to a mysql instance"
-        echo " - MySQL is still starting up and wasn't ready for connection yet"
-        echo " - The Mysql credentials were incorrect"
-        sleep 1;
-    done
-    echo "Setup Complete!"
-fi
+        cd /
 
-if [ "$CONFIG" == "1" ] &&
-   [ "$MANUAL_SETUP" != "yes" ] &&
-   [ "$EMPTY" != "yes" ]; then
-    # OpenEMR has been configured
-    if [ -f /var/www/localhost/htdocs/auto_configure.php ]; then
-        cd /var/www/localhost/htdocs/openemr/
-        # This section only runs once after above configuration since auto_configure.php gets removed after this script
-        echo "Setting user 'www' as owner of openemr/ and setting file/dir permissions to 400/500"
-        #set all directories to 500
-        find . -type d -print0 | xargs -0 chmod 500
-        #set all file access to 400
-        find . -type f -print0 | xargs -0 chmod 400
-
-        echo "Default file permissions and ownership set, allowing writing to specific directories"
-        chmod 700 /var/www/localhost/htdocs/run_openemr.sh
-        # Set file and directory permissions
-        chmod 600 interface/modules/zend_modules/config/application.config.php
-        find sites/default/documents -type d -print0 | xargs -0 chmod 700
-        find sites/default/edi -type d -print0 | xargs -0 chmod 700
-        find sites/default/era -type d -print0 | xargs -0 chmod 700
-        find sites/default/letter_templates -type d -print0 | xargs -0 chmod 700
-        find interface/main/calendar/modules/PostCalendar/pntemplates/cache -type d -print0 | xargs -0 chmod 700
-        find interface/main/calendar/modules/PostCalendar/pntemplates/compiled -type d -print0 | xargs -0 chmod 700
-        find gacl/admin/templates_c -type d -print0 | xargs -0 chmod 700
-
-        echo "Removing remaining setup scripts"
-        #remove all setup scripts
-        rm -f acl_setup.php
-        rm -f acl_upgrade.php
-        rm -f setup.php
-        rm -f sql_upgrade.php
-        rm -f ippf_upgrade.php
-        rm -f gacl/setup.php
-        echo "Setup scripts removed, we should be ready to go now!"
+        if [ "$FLEX_REPOSITORY_BRANCH" != "" ]; then
+            echo "Collecting $FLEX_REPOSITORY_BRANCH branch from $FLEX_REPOSITORY repository"
+            git clone "$FLEX_REPOSITORY" --branch "$FLEX_REPOSITORY_BRANCH" --depth 1
+        else
+            echo "Collecting $FLEX_REPOSITORY_TAG tag from $FLEX_REPOSITORY repository"
+            git clone "$FLEX_REPOSITORY"
+            cd openemr
+            git checkout "$FLEX_REPOSITORY_TAG"
+            cd ../
+        fi
+        rsync --recursive --links --exclude .git openemr /var/www/localhost/htdocs/
+        rm -fr openemr
         cd /var/www/localhost/htdocs/
     fi
+
+    if [ -f /var/www/localhost/htdocs/auto_configure.php ]; then
+        chmod 666 /var/www/localhost/htdocs/openemr/sites/default/sqlconf.php
+        chmod 666 /var/www/localhost/htdocs/openemr/interface/modules/zend_modules/config/application.config.php
+        chown -R apache /var/www/localhost/htdocs/openemr/
+    fi
+
+    CONFIG=$(php -r "require_once('/var/www/localhost/htdocs/openemr/sites/default/sqlconf.php'); echo \$config;")
+    if [ "$CONFIG" == "0" ] &&
+       [ "$MYSQL_HOST" != "" ] &&
+       [ "$MYSQL_ROOT_PASS" != "" ] &&
+       [ "$EMPTY" != "yes" ] &&
+       [ "$MANUAL_SETUP" != "yes" ]; then
+
+        echo "Running quick setup!"
+        while ! auto_setup; do
+            echo "Couldn't set up. Any of these reasons could be what's wrong:"
+            echo " - You didn't spin up a MySQL container or connect your OpenEMR container to a mysql instance"
+            echo " - MySQL is still starting up and wasn't ready for connection yet"
+            echo " - The Mysql credentials were incorrect"
+            sleep 1;
+        done
+        echo "Setup Complete!"
+    fi
+
+    if [ "$CONFIG" == "1" ] &&
+       [ "$MANUAL_SETUP" != "yes" ] &&
+       [ "$EMPTY" != "yes" ]; then
+        # OpenEMR has been configured
+        if [ -f /var/www/localhost/htdocs/auto_configure.php ]; then
+            cd /var/www/localhost/htdocs/openemr/
+            # This section only runs once after above configuration since auto_configure.php gets removed after this script
+            echo "Setting user 'www' as owner of openemr/ and setting file/dir permissions to 400/500"
+            #set all directories to 500
+            find . -type d -print0 | xargs -0 chmod 500
+            #set all file access to 400
+            find . -type f -print0 | xargs -0 chmod 400
+
+            echo "Default file permissions and ownership set, allowing writing to specific directories"
+            chmod 700 /var/www/localhost/htdocs/run_openemr.sh
+            # Set file and directory permissions
+            chmod 600 interface/modules/zend_modules/config/application.config.php
+            find sites/default/documents -type d -print0 | xargs -0 chmod 700
+            find sites/default/edi -type d -print0 | xargs -0 chmod 700
+            find sites/default/era -type d -print0 | xargs -0 chmod 700
+            find sites/default/letter_templates -type d -print0 | xargs -0 chmod 700
+            find interface/main/calendar/modules/PostCalendar/pntemplates/cache -type d -print0 | xargs -0 chmod 700
+            find interface/main/calendar/modules/PostCalendar/pntemplates/compiled -type d -print0 | xargs -0 chmod 700
+            find gacl/admin/templates_c -type d -print0 | xargs -0 chmod 700
+
+            echo "Removing remaining setup scripts"
+            #remove all setup scripts
+            rm -f acl_setup.php
+            rm -f acl_upgrade.php
+            rm -f setup.php
+            rm -f sql_upgrade.php
+            rm -f ippf_upgrade.php
+            rm -f gacl/setup.php
+            echo "Setup scripts removed, we should be ready to go now!"
+            cd /var/www/localhost/htdocs/
+        fi
+    fi
+
+    # ensure the auto_configure.php script has been removed
+    rm -f /var/www/localhost/htdocs/auto_configure.php
+fi
+
+if [ -f /etc/docker-leader ] &&
+   [ "$SWARM_MODE" == "yes" ]; then
+    # Set flag that the docker-leader configuration is complete
+    touch /var/www/localhost/htdocs/openemr/sites/default/docker-completed
 fi
 
 if [ "$REDIS_SERVER" != "" ] &&
@@ -182,6 +224,3 @@ if [ "$REDIS_SERVER" != "" ] &&
     # Ensure only configure this one time
     touch /etc/php-redis-configured
 fi
-
-# ensure the auto_configure.php script has been removed
-rm -f /var/www/localhost/htdocs/auto_configure.php
