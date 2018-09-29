1bcfa1e1bbd8805ed9f529e4285c5abf710e85c4
Asher Densmore-Lynn <jesdynf@gmail.com>
2018-09-28 23:33:26 -0500

    post-recovery backup fix

--- docker/mysql-xtrabackup/xbackup-wrapper.sh
+++ docker/mysql-xtrabackup/xbackup-wrapper.sh
@@ -9,6 +9,12 @@ if [ ! -f allsetup.ok ]; then
   exit 1
 fi
 
+[ if -f force-full-backup ]; then
+  rm force-full-backup
+  ./xbackup.sh -t full
+  exit $?
+fi
+
 # I don't like forcing it like this, but if the backup fails one day, we need to try it the next
 # here's the problem: manual run during an automated run will cause destruction and havoc and woe
 if [ $(date +%u) == 7 ]; then
