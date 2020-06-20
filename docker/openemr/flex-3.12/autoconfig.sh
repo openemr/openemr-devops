0f1b34d60089e5da1987d608820fc847737bb437
Julie <boxlady@gmail.com>
2020-06-20 15:18:53 -0700

    sql drive data import feature with some misc modularization

--- docker/openemr/flex-3.12/autoconfig.sh
+++ docker/openemr/flex-3.12/autoconfig.sh
@@ -16,6 +16,8 @@
 #      code from github (uses local repo).
 set -e
 
+source /root/devtoolsLibrary.source
+
 swarm_wait() {
     if [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-completed ]; then
         # true
@@ -27,35 +29,7 @@ swarm_wait() {
 }
 
 auto_setup() {
-
-    CONFIGURATION="server=${MYSQL_HOST} rootpass=${MYSQL_ROOT_PASS} loginhost=%"
-    if [ "$MYSQL_ROOT_USER" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} root=${MYSQL_ROOT_USER}"
-    fi
-    if [ "$MYSQL_USER" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} login=${MYSQL_USER}"
-        CUSTOM_USER="$MYSQL_USER"
-    else
-        CUSTOM_USER="openemr"
-    fi
-    if [ "$MYSQL_PASS" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} pass=${MYSQL_PASS}"
-        CUSTOM_PASSWORD="$MYSQL_PASS"
-    else
-        CUSTOM_PASSWORD="openemr"
-    fi
-    if [ "$MYSQL_DATABASE" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} dbname=${MYSQL_DATABASE}"
-        CUSTOM_DATABASE="$MYSQL_DATABASE"
-    else
-        CUSTOM_DATABASE="openemr"
-    fi
-    if [ "$OE_USER" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} iuser=${OE_USER}"
-    fi
-    if [ "$OE_PASS" != "" ]; then
-        CONFIGURATION="${CONFIGURATION} iuserpass=${OE_PASS}"
-    fi
+    prepareVariables
 
     if [ "$EASY_DEV_MODE" != "yes" ]; then
         chmod -R 600 /var/www/localhost/htdocs/openemr
@@ -70,19 +44,15 @@ auto_setup() {
         exit 2
     fi
 
-    # Set requested openemr settings
-    OPENEMR_SETTINGS=`printenv | grep '^OPENEMR_SETTING_'`
-    if [ -n "$OPENEMR_SETTINGS" ]; then
-        echo "$OPENEMR_SETTINGS" |
-        while IFS= read -r line; do
-            SETTING_TEMP=`echo "$line" | cut -d "=" -f 1`
-            # note am omitting the letter O on purpose
-            CORRECT_SETTING_TEMP=`echo "$SETTING_TEMP" | awk -F 'PENEMR_SETTING_' '{print $2}'`
-            VALUE_TEMP=`echo "$line" | cut -d "=" -f 2`
-            echo "Set ${CORRECT_SETTING_TEMP} to ${VALUE_TEMP}"
-            mysql -u "$CUSTOM_USER"  --password="$CUSTOM_PASSWORD" -h "$MYSQL_HOST" -e "UPDATE globals SET gl_value = '${VALUE_TEMP}' WHERE gl_name = '${CORRECT_SETTING_TEMP}'" "$CUSTOM_DATABASE"
-        done
+    if [ "$DEMO_MODE" == "standard" ]; then
+        demoData
     fi
+
+    if [ "$SQL_DATA_DRIVE" != "" ]; then
+        sqlDataDrive
+    fi
+
+    setGlobalSettings
 }
 
 if [ "$SWARM_MODE" == "yes" ]; then
