a281e940bb9f3cd37ad6b7b08b261e64d5a14f80
julie <boxlady@gmail.com>
2020-06-20 16:19:35 +0200

    Made some changes to allow a user to set the folder with LOAD_SQL_DATA_FROM: from docker compose. also using the variables that are passed from docker-compose. Note sure on what to do with deletion.

--- docker/openemr/flex-pt-data/autoconfig.sh
+++ docker/openemr/flex-pt-data/autoconfig.sh
@@ -334,14 +334,23 @@ if [ "$REDIS_SERVER" != "" ] &&
 fi
 
 if [ "$LOAD_SQL_DATA" == 1 ]; then
-    echo "Loading SQL DATA from";
-    echo $pwd;
-    cd  /var/www/localhost/htdocs/openemr/sql/loadin
+      echo "Loading SQL from";
+      #If a folder is set then use that... else use the default
+   if [ $LOAD_SQL_DATA_FROM != '' ]; then
+       cd $LOAD_SQL_DATA_FROM
+       echo $pwd;
+       else
+       cd  /var/www/localhost/htdocs/openemr/sql/loadin
+       echo $pwd;
+   fi
+   #Loop over all sql files inside of the current directory
     for f in *.sql ; do
       echo ${f};
-     mysql -u "$CUSTOM_USER"  --password="$CUSTOM_PASSWORD" -h "$MYSQL_HOST" "$CUSTOM_DATABASE" < $f
+      #Using varriables that are passed into docker-compose
+      mysql -u "$MYSQL_USER"  --password="$MYSQL_PASS" -h "$MYSQL_HOST" openemr < $f
       done
 fi
+
 
 if [ "$XDEBUG_IDE_KEY" != "" ] &&
    [ ! -f /etc/php-xdebug-configured ]; then
