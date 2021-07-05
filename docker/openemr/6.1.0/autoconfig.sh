fbd8a55d71c2eb26d787723131330a9c7ef974c2
Brady Miller <brady.g.miller@gmail.com>
2021-07-05 15:19:03 -0700

    random patient generator updates and php8 stuff (#296)

--- docker/openemr/6.1.0/autoconfig.sh
+++ docker/openemr/6.1.0/autoconfig.sh
@@ -220,8 +220,8 @@ fi
 if [ "$REDIS_SERVER" != "" ] &&
    [ ! -f /etc/php-redis-configured ]; then
     # Variable for $REDIS_SERVER is usually going to be something like 'redis'
-    sed -i "s@session.save_handler = files@session.save_handler = redis@" /etc/php7/php.ini
-    sed -i "s@;session.save_path = \"/tmp\"@session.save_path = \"tcp://$REDIS_SERVER:6379\"@" /etc/php7/php.ini
+    sed -i "s@session.save_handler = files@session.save_handler = redis@" /etc/php8/php.ini
+    sed -i "s@;session.save_path = \"/tmp\"@session.save_path = \"tcp://$REDIS_SERVER:6379\"@" /etc/php8/php.ini
     # Ensure only configure this one time
     touch /etc/php-redis-configured
 fi
