ca3eebb7d046ba1a4bfb7f25a9d2e68a667ab84b
Asher Densmore-Lynn <jesdynf@gmail.com>
2021-10-14 21:02:49 -0500

    opening move, untested

--- docker/openemr/6.1.0/openemr.sh
+++ docker/openemr/6.1.0/openemr.sh
@@ -36,6 +36,30 @@ auto_setup() {
     setGlobalSettings
 }
 
+# AUTHORITY is the right to change OpenEMR's configured state
+# - true for singletons, swarm leaders, and the Kubernetes startup job
+# - false for swarm members and Kubernetes workers
+# OPERATOR is the right to launch Apache and serve OpenEMR
+# - true for singletons, swarm members (leader or otherwise), and Kubernetes workers
+# - false for the Kubernetes startup job and manual image runs
+AUTHORITY=yes
+OPERATOR=yes
+if ["$K8S" == "admin"]; then
+    OPERATOR=no
+elif ["$K8S" == "worker"]; then
+    AUTHORITY=no
+fi
+if [ "$SWARM_MODE" == "yes" ]; then
+    # atomically test for leadership
+    set -o noclobber
+    { > /etc/docker-leader ; } &> /dev/null || AUTHORITY=no
+    set +o noclobber
+    
+    if [ "$AUTHORITY" == "yes" ]; then       
+        touch /var/www/localhost/htdocs/openemr/sites/default/docker-initiated                  
+    fi
+fi
+
 if [ "$SWARM_MODE" == "yes" ]; then
     # Check if the shared volumes have been emptied out (persistent volumes in
     # kubernetes seems to do this). If they have been emptied, then restore them.
@@ -50,14 +74,7 @@ if [ "$SWARM_MODE" == "yes" ]; then
         rsync --owner --group --perms --recursive --links /swarm-pieces/sites /var/www/localhost/htdocs/openemr/
     fi
 
-    # Need to support replication for docker orchestration
-    if [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-initiated ]; then
-        # This docker instance will be the leader and perform configuration
-        touch /var/www/localhost/htdocs/openemr/sites/default/docker-initiated
-        touch /etc/docker-leader
-    fi
-
-    if [ ! -f /etc/docker-leader ] &&
+    if ["$AUTHORITY" == "no"] &&
        [ ! -f /var/www/localhost/htdocs/openemr/sites/default/docker-completed ]; then
         while swarm_wait; do
             echo "Waiting for the docker-leader to finish configuration before proceeding."
@@ -66,55 +83,12 @@ if [ "$SWARM_MODE" == "yes" ]; then
     fi
 fi
 
-if [ -f /etc/docker-leader ] ||
-   [ "$SWARM_MODE" != "yes" ]; then
-    # ensure a self-signed cert has been generated and is referenced
-    if ! [ -f /etc/ssl/private/selfsigned.key.pem ]; then
-        openssl req -x509 -newkey rsa:4096 \
-        -keyout /etc/ssl/private/selfsigned.key.pem \
-        -out /etc/ssl/certs/selfsigned.cert.pem \
-        -days 365 -nodes \
-        -subj "/C=xx/ST=x/L=x/O=x/OU=x/CN=localhost"
-    fi
-    if [ ! -f /etc/ssl/docker-selfsigned-configured ]; then
-        rm -f /etc/ssl/certs/webserver.cert.pem
-        rm -f /etc/ssl/private/webserver.key.pem
-        ln -s /etc/ssl/certs/selfsigned.cert.pem /etc/ssl/certs/webserver.cert.pem
-        ln -s /etc/ssl/private/selfsigned.key.pem /etc/ssl/private/webserver.key.pem
-        touch /etc/ssl/docker-selfsigned-configured
-    fi
-
-    if [ "$DOMAIN" != "" ]; then
-            if [ "$EMAIL" != "" ]; then
-            EMAIL="-m $EMAIL"
-        else
-            echo "WARNING: SETTING AN EMAIL VIA \$EMAIL is HIGHLY RECOMMENDED IN ORDER TO"
-            echo "         RECEIVE ALERTS FROM LETSENCRYPT ABOUT YOUR SSL CERTIFICATE."
-        fi
-        # if a domain has been set, set up LE and target those certs
-
-        if ! [ -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
-            /usr/sbin/httpd -k start
-            sleep 2
-            certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d $DOMAIN $EMAIL --agree-tos
-            /usr/sbin/httpd -k stop
-            echo "1 23  *   *   *   certbot renew -q --post-hook \"httpd -k graceful\"" >> /etc/crontabs/root
-        fi
-
-        # run letsencrypt as a daemon and reference the correct cert
-        if [ ! -f /etc/ssl/docker-letsencrypt-configured ]; then
-            rm -f /etc/ssl/certs/webserver.cert.pem
-            rm -f /etc/ssl/private/webserver.key.pem
-            ln -s /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/ssl/certs/webserver.cert.pem
-            ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/ssl/private/webserver.key.pem
-            touch /etc/ssl/docker-letsencrypt-configured
-        fi
-    fi
+if ["$AUTHORITY" == "yes"]; then
+    sh ssl.sh
 fi
 
 UPGRADE_YES=false;
-if [ -f /etc/docker-leader ] ||
-   [ "$SWARM_MODE" != "yes" ]; then
+if ["$AUTHORITY" == "yes"]; then
     # Figure out if need to do upgrade
     if [ -f /root/docker-version ]; then
         DOCKER_VERSION_ROOT=$(cat /root/docker-version)
@@ -141,8 +115,15 @@ if [ -f /etc/docker-leader ] ||
 fi
 
 CONFIG=$(php -r "require_once('/var/www/localhost/htdocs/openemr/sites/default/sqlconf.php'); echo \$config;")
-if [ -f /etc/docker-leader ] ||
-   [ "$SWARM_MODE" != "yes" ]; then
+if ["$AUTHORITY" == "no"] &&
+    [ "$CONFIG" == "0" ]; then
+    echo "Critical failure! An OpenEMR worker is trying to run on a missing configuration."
+    echo " - Is this due to a Kubernetes grant hiccup?"
+    echo "The worker will now terminate."
+    exit 1
+fi
+
+if ["$AUTHORITY" == "yes"]; then
     if [ "$CONFIG" == "0" ] &&
        [ "$MYSQL_HOST" != "" ] &&
        [ "$MYSQL_ROOT_PASS" != "" ] &&
@@ -160,7 +141,9 @@ if [ -f /etc/docker-leader ] ||
     fi
 fi
 
-if [ "$CONFIG" == "1" ] &&
+if 
+   [ "$AUTHORITY" == "yes"] &&
+   [ "$CONFIG" == "1" ] &&
    [ "$MANUAL_SETUP" != "yes" ]; then
     # OpenEMR has been configured
 
@@ -202,14 +185,12 @@ if [ "$CONFIG" == "1" ] &&
         rm -f sql_patch.php
         rm -f sql_upgrade.php
         rm -f ippf_upgrade.php
+        rm -f auto_configure.php
         echo "Setup scripts removed, we should be ready to go now!"
     fi
 fi
 
-# ensure the auto_configure.php script has been removed
-rm -f auto_configure.php
-
-if [ -f /etc/docker-leader ] &&
+if [ "$AUTHORITY" == "yes"] &&
    [ "$SWARM_MODE" == "yes" ]; then
     # Set flag that the docker-leader configuration is complete
     touch /var/www/localhost/htdocs/openemr/sites/default/docker-completed
@@ -222,4 +203,17 @@ if [ "$REDIS_SERVER" != "" ] &&
     sed -i "s@;session.save_path = \"/tmp\"@session.save_path = \"tcp://$REDIS_SERVER:6379\"@" /etc/php8/php.ini
     # Ensure only configure this one time
     touch /etc/php-redis-configured
+fi
+
+echo ""
+echo "Love OpenEMR? You can now support the project via the open collective:"
+echo " > https://opencollective.com/openemr/donate"
+echo ""
+
+if [ "$OPERATOR" == "yes"]; then
+    echo "Starting apache!"
+    /usr/sbin/httpd -D FOREGROUND
+else
+    echo "OpenEMR configuration tasks have concluded."
+    exit 0
 fi
