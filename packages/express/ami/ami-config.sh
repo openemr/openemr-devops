db0d34fa7eee06fd43c22dca9e7e6d6992c0e151
Asher Densmore-Lynn <jesdynf@gmail.com>
2022-03-26 19:53:34 -0500

    Express bugfixes

--- packages/express/ami/ami-config.sh
+++ packages/express/ami/ami-config.sh
@@ -5,12 +5,11 @@ MYSQLROOTPWD="${1:-root}"
 f () {
     cd /root
     curl -s https://raw.githubusercontent.com/openemr/openemr-devops/master/packages/lightsail/launch.sh | bash -s -- -s 0    
-    docker exec -it $(docker ps | grep mysql | cut -f 1 -d " ") mysql --password="$MYSQLROOTPWD" -e "update openemr.users set active=0 where id=1;"
+    docker exec $(docker ps | grep mysql | cut -f 1 -d " ") mysql --password="$MYSQLROOTPWD" -e "update openemr.users set active=0 where id=1;"
     cp openemr-devops/packages/express/ami/ami-rekey.sh /etc/init.d/ami-rekey
     update-rc.d ami-rekey defaults
-    rm /root/.ssh/authorized_keys
-    rm /home/ubuntu/.ssh/authorized_keys
-    #rm /home/ubuntu/.bash_history
+    rm -f /root/.ssh/authorized_keys /home/ubuntu/.ssh/authorized_keys
+    rm -f /home/ubuntu/.bash_history
     sync
     shutdown -h now
     exit 0
