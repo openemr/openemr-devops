03d7d1ce5b931eab5a21b65866d7743b7af26dae
Brady Miller <brady.g.miller@gmail.com>
2023-01-24 12:51:44 -0800

    permission fix for odd bug in 7.0.0 docker ssl in kubernetes

--- docker/openemr/7.0.0/openemr.sh
+++ docker/openemr/7.0.0/openemr.sh
@@ -137,16 +137,22 @@ if [ -f /root/certs/mysql/server/mysql-ca ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca ]; then
     echo "copied over mysql-ca"
     cp /root/certs/mysql/server/mysql-ca /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca
 fi
 if [ -f /root/certs/mysql/server/mysql-cert ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert ]; then
     echo "copied over mysql-cert"
     cp /root/certs/mysql/server/mysql-cert /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert
 fi
 if [ -f /root/certs/mysql/server/mysql-key ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key ]; then
     echo "copied over mysql-key"
     cp /root/certs/mysql/server/mysql-key /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key
 fi
 if [ -f /root/certs/couchdb/couchdb-ca ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/couchdb-ca ]; then
