d9360b0287cfdf8b63f982a385f732c2e6a85a09
Brady Miller <brady.g.miller@gmail.com>
2023-01-24 18:13:32 -0800

    more fixes for odd bug in previous commit

--- docker/openemr/7.0.0/openemr.sh
+++ docker/openemr/7.0.0/openemr.sh
@@ -133,26 +133,29 @@ fi
 #    /root/certs/ldap/ldap-cert (supported)
 #    /root/certs/ldap/ldap-key (supported)
 #    /root/certs/redis/.. (not yet supported)
+MYSQLCA=false
 if [ -f /root/certs/mysql/server/mysql-ca ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca ]; then
     echo "copied over mysql-ca"
     cp /root/certs/mysql/server/mysql-ca /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca
-    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
-    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions at end of this script for it to work
+    MYSQLCA=true
 fi
+MYSQLCERT=false
 if [ -f /root/certs/mysql/server/mysql-cert ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert ]; then
     echo "copied over mysql-cert"
     cp /root/certs/mysql/server/mysql-cert /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert
-    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
-    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions at end of this script for it to work
+    MYSQLCERT=true
 fi
+MYSQLKEY=false
 if [ -f /root/certs/mysql/server/mysql-key ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key ]; then
     echo "copied over mysql-key"
     cp /root/certs/mysql/server/mysql-key /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key
-    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
-    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions at end of this script for it to work
+    MYSQLKEY=true
 fi
 if [ -f /root/certs/couchdb/couchdb-ca ] &&
    [ ! -f /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/couchdb-ca ]; then
@@ -250,6 +253,22 @@ if
         rm -f auto_configure.php
         echo "Setup scripts removed, we should be ready to go now!"
     fi
+fi
+
+if $MYSQLCA ; then
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    echo "adjusted permissions for mysql-ca"
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-ca
+fi
+if $MYSQLCERT ; then
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    echo "adjusted permissions for mysql-cert"
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-cert
+fi
+if $MYSQLKEY ; then
+    # for 7.0.0 docker specific issue in kubernetes, need to change permissions for it to work
+    echo "adjusted permissions for mysql-key"
+    chmod 744 /var/www/localhost/htdocs/openemr/sites/default/documents/certificates/mysql-key
 fi
 
 if [ "$AUTHORITY" == "yes" ] &&
