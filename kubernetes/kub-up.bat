6f4b329bac19c137034a1c9d2e1756c96bd84166
Brady Miller <brady.g.miller@gmail.com>
2023-01-31 23:02:27 -0800

    fix to support x509

--- kubernetes/kub-up.bat
+++ kubernetes/kub-up.bat
@@ -8,6 +8,7 @@ kubectl apply ^
     -f certs/ca-certificate.yaml ^
     -f certs/ca-issuer.yaml ^
     -f certs/mysql.yaml ^
+    -f certs/mysql-replication.yaml ^
     -f certs/mysql-openemr-client.yaml ^
     -f certs/phpmyadmin.yaml ^
     -f certs/mysql-phpmyadmin-client.yaml
