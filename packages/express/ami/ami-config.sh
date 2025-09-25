d9408c7624a69d6a90353b6216cc506f38861aee
Asher Densmore-Lynn <jesdynf@gmail.com>
2025-09-24 19:07:59 -0500

    answering

--- packages/express/ami/ami-config.sh
+++ packages/express/ami/ami-config.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e
 MYSQLROOTPWD="${1:-root}"
 
 f () {
@@ -7,19 +8,19 @@ f () {
     curl -s https://raw.githubusercontent.com/openemr/openemr-devops/master/packages/lightsail/launch.sh | bash -s -- -s 0
 
     # wait a while for services to build
-    until docker container ls | grep openemr/openemr >& /dev/null
+    until docker container ls | grep -q openemr/openemr
     do
         echo "waiting for container start..."
         sleep 5
     done
 
-    until docker top $(docker ps | grep _openemr | cut -f 1 -d " ") | grep httpd &> /dev/null
+    until docker top $(docker ps | grep -openemr | cut -f 1 -d " ") | grep httpd &> /dev/null
     do
         echo "waiting for service start..."
         sleep 20
     done
 
-    docker exec $(docker ps | grep mysql | cut -f 1 -d " ") mysql --password="${MYSQLROOTPWD}" -e "update openemr.users set active=0 where id=1;"
+    docker compose exec mysql mysql --password="${MYSQLROOTPWD}" -e "update openemr.users set active=0 where id=1;"
     cp openemr-devops/packages/express/ami/ami-rekey.sh /etc/init.d/ami-rekey
     chmod 755 /etc/init.d/ami-rekey
     update-rc.d ami-rekey defaults
