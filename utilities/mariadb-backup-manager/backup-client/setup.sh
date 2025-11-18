f3d1199c26ec6d8bf6ba1ec8d33552a0f543d9d4
Asher Densmore-Lynn <jesdynf@gmail.com>
2025-11-18 00:11:49 -0600

    narrowing

--- utilities/mariadb-backup-manager/backup-client/setup.sh
+++ utilities/mariadb-backup-manager/backup-client/setup.sh
@@ -10,37 +10,37 @@ source ./properties.secret
 echo "[client]" > root-credentials.conf
 chmod 600 root-credentials.conf
 
-if [[ -n "$MARIADB_USER_FROMSCRIPT" ]]; then
+if [[ -n "${MARIADB_USER_FROMSCRIPT}" ]]; then
 cat >> root-credentials.conf <<EOF
 user="${MARIADB_USER_FROMSCRIPT}"
 EOF
-elif [[ -n "$MARIADB_USER" ]]; then
+elif [[ -n "${MARIADB_USER}" ]]; then
 cat >> root-credentials.conf <<EOF
 user="${MARIADB_USER}"
 EOF
-elif [[ -n "$MYSQL_USER" ]]; then
+elif [[ -n "${MYSQL_USER}" ]]; then
 cat >> root-credentials.conf <<EOF
 user="${MYSQL_USER}"
 EOF
 fi
 
-if [[ -n "$MARIADB_PASSWORD_FROMSCRIPT" ]]; then
+if [[ -n "${MARIADB_PASSWORD_FROMSCRIPT}" ]]; then
 cat >> root-credentials.conf <<EOF
 password="${MARIADB_ROOT_PASSWORD}"
 EOF
-elif [[ -n "$MARIADB_PASSWORD_FROMSCRIPT" ]]; then
+elif [[ -n "${MARIADB_PASSWORD_FROMSCRIPT}" ]]; then
 cat >> root-credentials.conf <<EOF
 password="${MARIADB_ROOT_PASSWORD}"
 EOF
-elif [[ -n "$MYSQL_ROOT_PASSWORD" ]]; then
+elif [[ -n "${MYSQL_ROOT_PASSWORD}" ]]; then
 cat >> root-credentials.conf <<EOF
 password="${MYSQL_ROOT_PASSWORD}"
 EOF
-elif [[ -n "$MARIADB_PASSWORD" ]]; then
+elif [[ -n "${MARIADB_PASSWORD}" ]]; then
 cat >> root-credentials.conf <<EOF
 password="${MARIADB_PASSWORD}"
 EOF
-elif [[ -n "$MYSQL_PASSWORD" ]]; then
+elif [[ -n "${MYSQL_PASSWORD}" ]]; then
 cat >> root-credentials.conf <<EOF
 password="${MYSQL_PASSWORD}"
 EOF
